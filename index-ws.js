12f8e380350f23ef9e32dd07fef74b526ec449f6
Paolo <paolo@example.com>
2026-01-18 14:49:34 +0000

    Websockets

--- index-ws.js
+++ index-ws.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const server = require("http").createServer();
+const WebSocketServer = require("ws").Server;
 
 const app = express();
 
@@ -9,3 +10,27 @@ app.get("/", (req, res) => {
 
 server.on("request", app);
 server.listen(3000, () => console.log("Server started on port 3000");
+
+const wss = new WebSocketServer({ server: server });
+
+wss.on("connection", (ws) => {
+	const numClients = wss.clients.size;
+	console.log("Client connected", numClients);
+
+	wss.broadcast(`Current visitors: ${numClients}`);
+
+	if(ws.readyState === ws.OPEN) {
+		ws.send("Welcome to my server");
+	}
+
+	ws.on("close", () => {
+		wss.broadcast(`Current visitors: ${numClients}`);
+		console.log("A client has disconnected");
+	});
+});
+
+wss.broadcast = (data) => {
+	wss.clients.forEach(client => {
+		client.send(data);
+	});
+};
